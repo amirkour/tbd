822828cd7be3f50e2108eaeacf9376eea9464364
amir kouretchian <amirsgarbunk@gmail.com>
2015-12-22 13:20:45 -0800

    updating gruntfile to include browserify and watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,33 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
 
     grunt.initConfig({
+
+        // will provide linting and build-time hints/errors/warnings
+        // for the specified js files
         'jshint': {
             'files': ['Gruntfile.js', 'app.js', 'src/**/*.js']
+        },
+
+        // pack-up CJS-style modules into a bundle that'll get loaded/used
+        // in the browser
+        'browserify':{
+            'default':{
+                'src': 'src/graph-browser.js',
+                'dest': 'public/bundle.js'
+            }
+        },
+
+        // run 'grunt watch' to run the specified tasks over the specified
+        // files - will keep the grunt process running in the background.
+        'watch': {
+            'files': ['<%= jshint.files %>'],
+            'tasks': ['default']
         }
     });
 
-    // A very basic default task.
-    grunt.registerTask('default', 'Firing up the default task!', ['jshint']);
+    grunt.registerTask('default', ['jshint','browserify']);
 };
