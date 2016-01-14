1d4044cfead0142cc0e7e317cccf7390394af21c
amir kouretchian <amirsgarbunk@gmail.com>
2016-01-14 15:42:56 -0800

    adding graph-utils to graph-browser

--- src/graph-browser.js
+++ src/graph-browser.js
@@ -1,11 +1,13 @@
 // this script is used at buildtime by browserify to expose
 // node modules to the browser.
 
-var GraphNode = require('../lib/graph-node')
+var GraphUtils = require('../lib/graph-utils'),
+    GraphNode = require('../lib/graph-node')
     GraphEdge = require('../lib/graph-edge'),
     Graph = require('../lib/graph');
 
 (function(window){
+    window.GraphUtils = GraphUtils;
     window.GraphNode = GraphNode;
     window.GraphEdge = GraphEdge;
     window.Graph = Graph;
